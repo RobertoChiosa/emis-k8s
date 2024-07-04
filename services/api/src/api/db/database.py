1277d72e0a5d38cf6246b429b59dfe7487e9ca10
RobertoChiosa <roberto.chiosa@gmail.com>
2024-07-05 00:42:25 +0200

    succesfully connected api and db

--- services/api/src/api/db/database.py
+++ services/api/src/api/db/database.py
@@ -3,11 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/monitoring"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'sslmode': 'disable'})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()

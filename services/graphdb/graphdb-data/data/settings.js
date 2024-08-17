2d94f1c84afb959518f2930537f4ac9412f2b896
RobertoChiosa <roberto.chiosa@gmail.com>
2024-08-17 14:36:42 +0200

    compose with graphdb and postgres db

---
+++ services/graphdb/graphdb-data/data/settings.js
@@ -0,0 +1,36 @@
+{
+  "properties" : {
+    "current.location" : ""
+  },
+  "import.local" : {
+    "bos;;polito.ttl" : {
+      "name" : "polito.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/polito.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "594b7453-a961-4b0c-943d-ff03a95a88f5",
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true,
+        "contextLink" : null
+      },
+      "size" : "62.91 KB",
+      "lastModified" : 1723818762000,
+      "imported" : 1723818762942,
+      "addedStatements" : 1877,
+      "removedStatements" : 0,
+      "numReplacedGraphs" : 0
+    }
+  }
+}
--- services/postgres/init.sql
+++ services/postgres/init.sql
@@ -1,3 +1,4 @@
+-- Create table
 CREATE TABLE data
 (
     time  TIMESTAMPTZ      NOT NULL,
@@ -5,14 +6,17 @@ CREATE TABLE data
     uuid  VARCHAR
 );
 
+-- Add timescale extension
 CREATE extension if not exists timescaledb;
 
+-- Create hypertable to manage timeseries data
 SELECT create_hypertable('data', 'time',
                          if_not_exists => TRUE,
                          migrate_data => TRUE,
                          create_default_indexes => TRUE,
                          chunk_time_interval => interval '1 day');
 
+-- Create materialized view for continuous aggregate
 CREATE MATERIALIZED VIEW data_1h
     WITH (timescaledb.continuous) AS
 SELECT time_bucket('1 h', time) AS bucket,
